f5888b8960e2f2324f38006072061b7295cd0199
Davide Papagni <davide@algonauti.com>
2019-06-12 15:41:49 +0200

    update ActiveStorage.upload() to compute checksum

--- lib/active_storage.dart
+++ lib/active_storage.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:meta/meta.dart';
 
 import 'src/direct_upload.dart';
+import 'src/file_checksum.dart';
 import 'src/uploader.dart';
 
 export 'src/direct_upload.dart';
@@ -19,12 +20,13 @@ class ActiveStorage {
   }) async {
     final uploader = Uploader(directUploadURL);
     int fileSize = await file.length();
+    String checksum = await FileChecksum.getMd5AsBase64(file: file);
     DirectUploadResponse response = await uploader.directUpload(
       DirectUploadRequest(
         fileName: fileName,
         contentType: fileMimeType,
         byteSize: fileSize,
-        fileContents: file.openRead(),
+        checksum: checksum,
       ),
     );
     await uploader.fileUpload(
