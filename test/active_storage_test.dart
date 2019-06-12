882f294fa4c596266e5506a13c771d5c29b39de6
Davide Papagni <davide@algonauti.com>
2019-06-12 19:28:11 +0200

    ActiveStorage: update upload() method test to use test server

--- test/active_storage_test.dart
+++ test/active_storage_test.dart
@@ -3,19 +3,24 @@ import "dart:io";
 import 'package:active_storage/active_storage.dart';
 import 'package:test/test.dart';
 
+import 'utils.dart';
+
 void main() {
-  test('returns DirectUploadResponse', () async {
-    var activeStorage =
-        ActiveStorage(directUploadURL: 'http://example.com/upload');
-    File file = File('test/sample.txt');
+  test('upload() returns DirectUploadResponse', () async {
     int progress;
-    var response = await activeStorage.upload(
-      fileName: 'sample',
-      fileMimeType: 'text/plain',
-      file: file,
-      onProgress: (percent) => progress = percent,
-    );
-    expect(response, new TypeMatcher<DirectUploadResponse>());
-    expect(progress, 100);
+    return startServer().then((_) {
+      var activeStorage =
+          ActiveStorage(directUploadURL: '$serverUrl/direct-upload');
+      File file = File('test/sample.txt');
+      return activeStorage.upload(
+        fileName: 'sample',
+        fileMimeType: 'text/plain',
+        file: file,
+        onProgress: (percent) => progress = percent,
+      );
+    }).then((DirectUploadResponse response) {
+      expect(response, new TypeMatcher<DirectUploadResponse>());
+      expect(progress, 100);
+    }).whenComplete(stopServer);
   });
 }
