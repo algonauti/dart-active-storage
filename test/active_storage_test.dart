4f5df21d5ea39760cfac1b997c7b7d01db681868
Davide Papagni <davide@algonauti.com>
2019-06-12 15:42:42 +0200

    fix outdated variable name and description in ActiveStorage.upload() test

--- test/active_storage_test.dart
+++ test/active_storage_test.dart
@@ -4,17 +4,17 @@ import 'package:active_storage/active_storage.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('returns ActiveStorageBlob', () async {
+  test('returns DirectUploadResponse', () async {
     File file = File('test/sample.txt');
     int progress;
-    var blob = await ActiveStorage.upload(
+    var response = await ActiveStorage.upload(
       fileName: 'sample',
       fileMimeType: 'text/plain',
       file: file,
       directUploadURL: 'http://example.com/upload',
       onProgress: (percent) => progress = percent,
     );
-    expect(blob, new TypeMatcher<DirectUploadResponse>());
+    expect(response, new TypeMatcher<DirectUploadResponse>());
     expect(progress, 100);
   });
 }
