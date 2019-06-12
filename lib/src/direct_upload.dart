01722d988c7df2c0f2a3a5b31747ce12286c0ff1
Davide Papagni <davide@algonauti.com>
2019-06-12 19:15:48 +0200

    DirectUploadResponse: add constructors (+)
    - default constructor
    - fromJson() factory constructor

--- lib/src/direct_upload.dart
+++ lib/src/direct_upload.dart
@@ -1,12 +1,32 @@
+import 'dart:convert';
+
 import 'package:meta/meta.dart';
 
 class DirectUploadResponse {
-  String id;
-  String key;
-  String signedId;
-  String uploadUrl;
-  Map<String, String> headers;
-// TODO Constructor
+  final int id;
+  final String key;
+  final String signedId;
+  final String uploadUrl;
+  final Map<String, String> headers;
+
+  DirectUploadResponse({
+    @required this.id,
+    @required this.key,
+    @required this.signedId,
+    @required this.uploadUrl,
+    @required this.headers,
+  });
+
+  factory DirectUploadResponse.fromJson(String utf8Text) {
+    var parsed = json.decode(utf8Text);
+    return DirectUploadResponse(
+      id: parsed['id'],
+      key: parsed['key'],
+      signedId: parsed['signed_id'],
+      uploadUrl: parsed['direct_upload']['url'],
+      headers: Map<String, String>.from(parsed['direct_upload']['headers']),
+    );
+  }
 }
 
 class DirectUploadRequest {
