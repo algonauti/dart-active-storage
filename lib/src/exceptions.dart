d8b76bd1b44a9a34fb96304084b136154a3ac351
Davide Papagni <davide@algonauti.com>
2019-10-08 12:51:50 +0200

    add http/network custom exceptions

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -2,3 +2,67 @@ class FileChecksumException implements Exception {
   final String message;
   FileChecksumException(this.message);
 }
+
+class HttpStatusException implements Exception {
+  String method;
+  String url;
+  String responseBody;
+  String requestBody;
+  int statusCode;
+
+  HttpStatusException(
+    this.method,
+    this.url, {
+    this.statusCode,
+    this.requestBody,
+    this.responseBody,
+  });
+
+  @override
+  String toString() =>
+      "HTTP error: $method $url\n\n$requestBody\n$responseBody";
+}
+
+class ClientError extends HttpStatusException {
+  ClientError(
+    String method,
+    String url,
+    int statusCode,
+    String requestBody,
+    String responseBody,
+  ) : super(
+          method,
+          url,
+          statusCode: statusCode,
+          requestBody: requestBody,
+          responseBody: responseBody,
+        );
+
+  @override
+  String toString() =>
+      "Client request error: $method $url\n\n$requestBody\n$responseBody";
+}
+
+class ServerError extends HttpStatusException {
+  ServerError(
+    String method,
+    String url,
+    int statusCode,
+    String requestBody,
+    String responseBody,
+  ) : super(
+          method,
+          url,
+          statusCode: statusCode,
+          requestBody: requestBody,
+          responseBody: responseBody,
+        );
+
+  @override
+  String toString() =>
+      "Server processing error: $method $url\n\n$requestBody\n$responseBody";
+}
+
+class NoNetworkError implements Exception {}
+
+class NetworkError implements Exception {}
